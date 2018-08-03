5a3cdd9df85a6e7a7d1545ab607149d7e5292263
Jose Barron Tovar <josebarrontovar@hotmail.com>
2018-08-03 16:13:38 -0500

    tweet2

--- src/app/components/time-line/time-line.component.ts
+++ src/app/components/time-line/time-line.component.ts
@@ -7,7 +7,13 @@ import {TimeLineService} from '../../service/timeLine/time-line.service';
   styleUrls: ['./time-line.component.css']
 })
 export class TimeLineComponent implements OnInit {
+   closeResult: string;
   Twetts=[];
+  mensaje:string;
+  usuarioSeleccionado = "Default";
+  usuarioReceptor: any;
+
+
   constructor(private MesagesDatasService: TimeLineService) { }
 
   ngOnInit() {
@@ -17,5 +23,11 @@ export class TimeLineComponent implements OnInit {
   }
 
  
+  public  postMensaje(){
+    this.usuarioReceptor= localStorage.getItem("IdReceptor");
+    this.MesagesDatasService.postMensajes(this.mensaje);
+    this.mensaje='';
+   
+  }
 
 }
