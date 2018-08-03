2940cb74a2fda3c09ef2cb7f40eb404eced93dbd
RicardoSTovar <ricardo.sauceda95@gmail.com>
2018-08-03 14:40:57 -0500

    TimeLineListado

--- src/app/components/time-line/time-line.component.ts
+++ src/app/components/time-line/time-line.component.ts
@@ -12,4 +12,11 @@ export class TimeLineComponent implements OnInit {
   ngOnInit() {
   }
 
+  Twetts=[
+    {nombre:'Horacio Chairez',Contenido:'Este es un Texto harcodeado el cual solo es un template Este es un Texto harcodeado el cual solo es un template',img:'https://avatars0.githubusercontent.com/u/26940519?s=460&v=4'},
+    {nombre:'Ricardo Sauceda',Contenido:'Este es un Texto harcodeado el cual solo es un template Este es un Texto harcodeado el cual solo es un template',img:'https://avatars0.githubusercontent.com/u/29348368?s=400&u=86c5d5cbde50f5f0f4c52be07c2d8dc7b668fc79&v=4'},
+    {nombre:'Camilo Orrante',Contenido:'Este es un Texto harcodeado el cual solo es un template Este es un Texto harcodeado el cual solo es un template',img:'https://avatars0.githubusercontent.com/u/26940519?s=460&v=4'},
+    {nombre:'Ricardo Sauceda',Contenido:'Este es un Texto harcodeado el cual solo es un template Este es un Texto harcodeado el cual solo es un template',img:'https://avatars0.githubusercontent.com/u/29348368?s=400&u=86c5d5cbde50f5f0f4c52be07c2d8dc7b668fc79&v=4'}
+  ]
+
 }
